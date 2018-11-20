c4f4f99a690fc0320235c75f7d291e34cf5285f3
= <ekirapapaul@gmail.com>
2018-11-20 14:12:52 +0300

    [UPDATE] gitignre

--- connector.py
+++ connector.py
@@ -3,17 +3,19 @@ import time
 import schedule
 from constants import KICKOFF_TYPE_ID, ENROLLMENT_TYPE_ID, APPOINTMENT_REMINDER_TYPE_ID
 from SendMessage import SendMessage
+from ConnectorUtils import ConnectorUtils
 
 
 if __name__ == "__main__":
-    schedule.every(5).minutes.do(SendMessage(ENROLLMENT_TYPE_ID).broadcast_message())
-    schedule.every(5).minutes.do(SendMessage(KICKOFF_TYPE_ID).broadcast_message())
-    schedule.every(5).minutes.do(SendMessage(APPOINTMENT_REMINDER_TYPE_ID).broadcast_message())
+    #SendMessage(ENROLLMENT_TYPE_ID).broadcast_message()
+    schedule.every(5).minutes.do(SendMessage(ENROLLMENT_TYPE_ID).broadcast_message)
+    schedule.every(5).minutes.do(SendMessage(KICKOFF_TYPE_ID).broadcast_message)
+    schedule.every(5).minutes.do(SendMessage(APPOINTMENT_REMINDER_TYPE_ID).broadcast_message)
     while True:
         schedule.run_pending()
         time.sleep(1)
 
     #create_contact()
-    #print ConnectorUtils().get_last_checked(ENROLLMENT_TYPE_ID)
+    print ConnectorUtils().get_last_checked(ENROLLMENT_TYPE_ID)
     #SendMessage(ENROLLMENT_TYPE_ID).broadcast_message()
     #SendMessage(KICKOFF_TYPE_ID).broadcast_message()
